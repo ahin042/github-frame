1a2afc4d5b35e682759c0050037ad3f4d191fb2b
lhr0920 <leongkfkd0920@gmail.com>
2026-08-04 18:36:56 +0900

    네온 효과 추가

--- popup.js
+++ popup.js
@@ -1,8 +1,24 @@
 const frame_color = document.getElementById('color');
-const frame_neon = document.getElementById('neon');
+const frame_effect = document.getElementById('effect');
 const profile = document.getElementById('show_profile')
 
 frame_color.addEventListener('input', () => {
-    profile.style.borderColor = frame_color.value;
     profile.style.border = `3px solid ${frame_color.value}`;
+    profile.style.borderRadius = '50%';
+
+    if (frame_effect.checked) {
+        profile.style.boxShadow = `
+        0 0 3px #fff, 0 0 5px ${frame_color.value}, 0 0 10px ${frame_color.value}, 0 0 20px ${frame_color.value}, 0 0 40px ${frame_color.value}`;
+    } else {
+        profile.style.boxShadow = 'none';
+    }
 }); 
+
+frame_effect.addEventListener('change', () => {
+    if (frame_effect.checked) {
+        profile.style.boxShadow = `
+            0 0 3px #fff, 0 0 5px ${frame_color.value}, 0 0 10px ${frame_color.value}, 0 0 20px ${frame_color.value}, 0 0 40px ${frame_color.value}`;
+    } else {
+        profile.style.boxShadow = 'none';
+    }
+})
